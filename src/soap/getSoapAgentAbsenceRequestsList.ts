21419e762ba9780777a25f076acc935b03aa5c12
remi-remi <remiveritepro@gmail.com>
2026-06-10 14:40:10 +0200

    Changes to be committed: 	modified:   src/soap/getSoapAgentAbsenceRequestsList.ts

--- src/soap/getSoapAgentAbsenceRequestsList.ts
+++ src/soap/getSoapAgentAbsenceRequestsList.ts
@@ -41,8 +41,11 @@ export const getSoapAgentAbsenceRequestsList = async function () {
    if (faultMatch)
       throw new Error(`kelio error response: ${faultMatch[1]}`);
 
-   if (!xml.includes('</ns1:exportAbsenceRequestsList>'))
+   if (!xml.includes('</ns1:exportAbsenceRequestsList>')) {
+      logger.debug('missing </ns1:exportAbsenceRequestsList> in xml :')
+      logger.debug(xml)
       throw new Error('kelio responded with wrong format, missing "</ns1:exportAbsenceRequestsList>"');
+   }
 
    return (xml)
 }
